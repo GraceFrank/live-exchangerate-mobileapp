9ea29f2560ffa153782c3ba7137685dd9eadfba2
Grace <frankgracey@gmail.com>
2021-11-03 10:53:08 +0100

    connected to api

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { StyleSheet, Text, View } from "react-native";
+import { render } from "react-dom";
+// import { API_BASE_URL } from "react-native-dotenv";
+const socket = io.connect("http://localhost:5000/");
 
 export default function App() {
+  const [exchangeRates, setExchangeRates] = useState([]);
+
+  const renderExchangeRates = exchangeRates.map((rate) => {
+    return <Text>{rate.timestamp}</Text>;
+  });
+
+  useEffect(() => {
+    socket.on("exchangeRateUpdate", (payload) => {
+      setExchangeRates(payload);
+      console.log(payload);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Expo App {"API_BASE_URL"}</Text>
       <StatusBar style="auto" />
+      {renderExchangeRates}
     </View>
   );
 }
@@ -14,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
