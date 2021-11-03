0bcc6827c82b81dc523b6ea517a5179b2f837740
Grace <frankgracey@gmail.com>
2021-11-03 12:50:38 +0100

    added styling and rendering of list

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import io from "socket.io-client";
 import { StyleSheet, Text, View } from "react-native";
+import { ListItem, Avatar, Header } from "react-native-elements";
+import images from "./assets/images";
+
 import { render } from "react-dom";
 // import { API_BASE_URL } from "react-native-dotenv";
 const socket = io.connect("http://localhost:5000/");
@@ -9,8 +13,18 @@ const socket = io.connect("http://localhost:5000/");
 export default function App() {
   const [exchangeRates, setExchangeRates] = useState([]);
 
-  const renderExchangeRates = exchangeRates.map((rate) => {
-    return <Text>{rate.timestamp}</Text>;
+  const renderExchangeRates = exchangeRates.map(({ ticker }, i) => {
+    return (
+      <ListItem key={i} bottomDivider>
+        <Avatar source={images[ticker.base]} />
+        <ListItem.Content>
+          <ListItem.Title>{ticker.base}</ListItem.Title>
+          <ListItem.Subtitle>
+            ${Number(ticker.price).toLocaleString()}
+          </ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
   });
 
   useEffect(() => {
@@ -21,19 +35,29 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Text>Expo App {"API_BASE_URL"}</Text>
-      <StatusBar style="auto" />
-      {renderExchangeRates}
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Header
+          style={styles.header}
+          centerComponent={{ text: "Exchange Rates", style: { color: "#fff" } }}
+          rightComponent={{ icon: "home", color: "#fff" }}
+          containerStyle={styles.header}
+        />
+        <StatusBar style="auto" />
+        {renderExchangeRates}
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "black",
+    color: "white",
+  },
+  header: {
+    backgroundColor: "black",
+    fontWeight: "bold",
   },
 });
---
+++ assets/images.js
@@ -0,0 +1,7 @@
+export default {
+  BTC: require("../node_modules/cryptocurrency-icons/svg/color/btc.svg"),
+  ETH: require("../node_modules/cryptocurrency-icons/svg/color/eth.svg"),
+  DOT: require("../node_modules/cryptocurrency-icons/svg/color/dot.svg"),
+  BNB: require("../node_modules/cryptocurrency-icons/svg/color/bnb.svg"),
+  BNB: require("../node_modules/cryptocurrency-icons/svg/color/ada.svg"),
+};
